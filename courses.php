427ed1a1c61af54f0e4a6a510e1cdad66fba469a
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-04-15 17:34:23 +0200

    Small changes

--- courses.php
+++ courses.php
@@ -4,6 +4,7 @@ require_once "init.php";
 $templateParams["title"] = "Courses";
 $templateParams["content"] = "courses_content.php";
 $templateParams["style"] = ["style.css"];
+$templateParams["degrees"] = $dbh->getDegrees();
 
 require "template/base.php";
 ?>
