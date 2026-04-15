c8f0dc9d5087cedc72e94c6ca786494156c725a9
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-04-15 21:36:00 +0200

    Minor changes

--- professors.php
+++ professors.php
@@ -4,6 +4,7 @@ require_once "init.php";
 $templateParams["title"] = "Professors";
 $templateParams["content"] = "professors_content.php";
 $templateParams["style"] = ["style.css"];
+$templateParams["degrees"] = $dbh->getDegrees();
 
 require "template/base.php";
 ?>
