81ed4c564d402f3ab2a281612ed4ac0f1b29e477
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-04-25 21:21:53 +0200

    Added a new parameter

--- course.php
+++ course.php
@@ -5,5 +5,9 @@ $templateParams["title"] = "Course";
 $templateParams["content"] = "course_content.php";
 $templateParams["style"] = ["style.css"];
 
+if (isset($_GET["course"])) {
+    $templateParams["course"] = $_GET["course"];
+};
+
 require "template/base.php";
 ?>
