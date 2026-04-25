c02a96e671c1a480b23459ec3e01e4e0b312b0c4
Davide Mancini <davide.mancini10@studio.unibo.it>
2026-04-25 20:38:06 +0200

    Added login api server side

--- login.php
+++ login.php
@@ -1,6 +1,16 @@
 <?php
 require_once "init.php";
 
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    $loginResult = $dbh->checkLogin($_POST["username"], $_POST["password"]);
+    if (count($loginResult) > 0) {
+        registerLoggedUser($loginResult[0]);
+        header("location: index.php");
+    } else {
+        $templateParams["loginError"] = "Username or password wrong";
+    }
+}
+
 $templateParams["title"] = "Login";
 $templateParams["content"] = "login_content.php";
 $templateParams["style"] = ["style.css", "login.css"];
