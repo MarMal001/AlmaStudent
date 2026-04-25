59de78bde33ede36a9d19ece8105d8b3028a26d9
Davide Mancini <davide.mancini10@studio.unibo.it>
2026-04-25 20:40:51 +0200

    Added auto redirecting to login if user is not logged int

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 require_once "init.php";
 
+if (!isUserLoggedIn()) {
+    header("location: login.php");
+}
+
 $templateParams["title"] = "Home";
 $templateParams["content"] = "home.php";
 $templateParams["style"] = ["style.css"];
