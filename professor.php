13d242ff5b4104451ccd47f4e025cf4733eed7f6
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-04-25 21:19:33 +0200

    Added a new parameter

--- professor.php
+++ professor.php
@@ -5,5 +5,9 @@ $templateParams["title"] = "Professor";
 $templateParams["content"] = "professor_content.php";
 $templateParams["style"] = ["style.css"];
 
+if (isset($_GET["professor"])) {
+    $templateParams["professor"] = $_GET["professor"];
+};
+
 require "template/base.php";
 ?>
