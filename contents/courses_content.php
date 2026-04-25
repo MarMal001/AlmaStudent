d1f818987a1f7428c34f7b40164988578f371b1c
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-04-25 21:17:06 +0200

    Added reference to a link

--- contents/courses_content.php
+++ contents/courses_content.php
@@ -34,7 +34,7 @@
                         <?php $professors = $dbh->getProfessorsByCourse($courseId); ?>
                             <ul class="d-flex flex-column align-items-start">
                             <?php foreach($professors as $professor): ?>
-                                <li><a href="#" class="text-primary"><?php echo $professor["name"] . " " . $professor["surname"] ?></a></li>
+                                <li><a href="professor.php?professor=<?php echo $professor["professor"]; ?>" class="text-primary"><?php echo $professor["name"] . " " . $professor["surname"] ?></a></li>
                             <?php endforeach; ?>
                             </ul>
                         <p><?php echo $course["shDescription"]; ?></p>
