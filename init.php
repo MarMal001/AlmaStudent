b5e1597390b7a8fdf40cb91b667130ea726ba3df
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-04-30 18:46:31 +0200

    Added useful variables

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 define("UPLOAD_DIR", "./images/");
+define("SERVER_ROOT", "/AlmaStudent/");
 
 define("RECEPTION_ACTION_ADD", 0);
 define("RECEPTION_ACTION_MODIFY", 1);
@@ -10,4 +11,14 @@ require_once("utils/functions.php");
 require_once("db/database.php");
 
 $dbh = new DatabaseHelper("localhost", "root", "", "AlmaStudent_ER", 3306);
+
+if (!isUserLoggedIn() && $_SERVER['PHP_SELF'] != SERVER_ROOT . "login.php" && $_SERVER['PHP_SELF'] != SERVER_ROOT . "create_account.php") {
+    header("location: login.php");
+}
+
+if (isUserLoggedIn()) {
+    $user = $_SESSION["username"];
+    $role = $_SESSION["role"];
+}
+
 ?>
