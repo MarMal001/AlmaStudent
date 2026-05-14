2925560f79713f409f220c53fb9b7ee1e3095d5f
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-05-14 12:32:52 +0200

    Added redirection of admin api based on action needed

--- admin_modify.php
+++ admin_modify.php
@@ -1,8 +1,22 @@
 <?php
 require_once "init.php";
 
+define("ADD_COURSE", 0);
+define("ADD_ACCOUNT", 1);
+
+if (!isset($_GET["type"])) {
+    header("location: index.php");
+}
+
 $templateParams["title"] = "Create account";
-$templateParams["content"] = "admin_modify_content.php";
+
+if ($_GET["type"] == "addAccount") {
+    $templateParams["content"] = "add_account.php";
+} else if ($_GET["type"] == "addCourse") {
+    $templateParams["degrees"] = $dbh->getDegrees();
+    $templateParams["content"] = "add_course.php";
+}
+
 $templateParams["style"] = ["style.css"];
 
 if (isset($_GET["message"])) {
--- handle_admin.php
+++ handle_admin.php
@@ -3,37 +3,65 @@
 require_once "init.php";
 
 if (!isAdmin()) {
-    header("location: login.php");
+    header("location: /");
 }
 
-if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["type"])) {
-    if ($_POST["type"] == "admin" || $_POST["type"] == "professor") {
-        if (isset($_POST["department"]) && isset($_POST["seat"]) && isset($_POST["infoReception"])) {
-            $department = $_POST["department"];
-            $seat = $_POST["seat"];
-            $infoReception = $_POST["infoReception"];
-
-            if (isset($_FILES["profilePicture"])) {
-                $profilePicture = UPLOAD_DIR . "professor/" . $_POST["username"] . ".png";
-                $f = fopen($profilePicture, "wb");
-                fwrite($f, file_get_contents($_FILES["profilePicture"]["tmp_name"]));
+switch ($_POST["action"]) {
+    case ADD_ACCOUNT:
+        if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["type"])) {
+            if ($_POST["type"] == "admin" || $_POST["type"] == "professor") {
+                add_account($_POST["username"], $_POST["password"], $_POST["name"], $_POST["surname"], $_POST["type"]);
             } else {
-                $profilePicture = "default.png";
+                $GLOBALS["message"] = "Tipo di account invalido";
             }
         } else {
-            $department = NULL;
-            $seat = NULL;
-            $infoReception = NULL;
-            $profilePicture = NULL;
+            $GLOBALS["message"] = "Parametri mancanti";
         }
-
-        if ($dbh->createAccout($_POST["username"], $_POST["password"], $_POST["name"], $_POST["surname"], $_POST["type"], department: $department, seat: $seat, infoReception: $infoReception, profilePicture: $profilePicture)) {
-            $message = "Account aggiunto con successo";
+        break;
+    case ADD_COURSE:
+        if (isset($_POST["degreeCode"]) && isset($_POST["name"]) && isset($_POST["year"]) && isset($_POST["semester"]) && isset($_POST["professors"]) && isset($_POST["courseId"])) {
+            add_course($_POST["degreeCode"], $_POST["name"], $_POST["year"], $_POST["semester"], $_POST["professors"], $_POST["courseId"]);
         } else {
-            $message = "Non è stato possibile aggiungere l'account";
+            $GLOBALS["message"] = "Parametri mancanti";
+        }
+        break;
+    default:
+        $GLOBALS["message"] = "Azione sconosciuta: " . $_POST["action"];
+        break;
+}
+
+function add_account($username, $password, $name, $surname, $type) {
+    if (isset($_POST["department"]) && isset($_POST["seat"]) && isset($_POST["infoReception"])) {
+        $department = $_POST["department"];
+        $seat = $_POST["seat"];
+        $infoReception = $_POST["infoReception"];
+
+        if (isset($_FILES["profilePicture"])) {
+            $profilePicture = UPLOAD_DIR . "professor/" . $username . ".png";
+            $f = fopen($profilePicture, "wb");
+            fwrite($f, file_get_contents($_FILES["profilePicture"]["tmp_name"]));
+        } else {
+            $profilePicture = "default.png";
         }
     } else {
-        $message = "Azione sconosciuta: " . $_POST["action"];
+        $department = NULL;
+        $seat = NULL;
+        $infoReception = NULL;
+        $profilePicture = NULL;
+    }
+
+    if ($GLOBALS["dbh"]->createAccout($username, $password, $name, $surname, $type, department: $department, seat: $seat, infoReception: $infoReception, profilePicture: $profilePicture)) {
+        $GLOBALS["message"] = "Account aggiunto con successo";
+    } else {
+        $GLOBALS["message"] = "Non è stato possibile aggiungere l'account";
+    }
+}
+
+function add_course($degreeCode, $name, $year, $semester, $professors, $courseId) {
+    if ($GLOBALS["dbh"]->addCourse($degreeCode, $name, $year, $semester, $professors, $courseId)) {
+        $GLOBALS["message"] = "Corso aggiunto con successo";
+    } else {
+        $GLOBALS["message"] = "Non è stato possibile aggiungere il corso";
     }
 }
 
