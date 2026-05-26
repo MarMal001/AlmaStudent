93bebc961de0c15bcf1fad282f4690fa51d4eba9
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-05-26 18:55:25 +0200

    courses.php Add: added javascript file to create dynamic page based on degree course selected

--- courses.php
+++ courses.php
@@ -5,7 +5,7 @@ $templateParams["title"] = "Courses";
 $templateParams["content"] = "courses_content.php";
 $templateParams["style"] = ["style.css"];
 $templateParams["degrees"] = $dbh->getDegrees();
-$templateParams["js"] = array();
+$templateParams["js"] = array("js/degrees.js");
 
 require "template/base.php";
 ?>
