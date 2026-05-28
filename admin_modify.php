5f8409836522457bbc8127fc226dfc116886bff3
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-05-28 13:40:20 +0200

    admin_modify.php handle_degrees.php Add: added code to generate form for updating a degree dynamically with ajax

--- admin_modify.php
+++ admin_modify.php
@@ -13,10 +13,12 @@ if ($_GET["type"] == "addAccount") {
     $templateParams["degrees"] = $dbh->getDegrees();
     $templateParams["content"] = "add_course.php";
 } else if ($_GET["type"] == "handleDegrees") {
+    $templateParams["degrees"] = $dbh->getDegrees();
     $templateParams["content"] = "handle_degrees.php";
 }
 
 $templateParams["style"] = ["style.css"];
+$templateParams["js"] = array("js/degrees.js");
 
 if (isset($_GET["message"])) {
     $templateParams["message"] = $_GET["message"];
