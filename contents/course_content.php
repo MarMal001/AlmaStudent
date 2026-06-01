edad4e3cf91f0cb5efd4aafda98126a4400d3961
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-06-01 22:32:40 +0200

    Removed direct use of js to create a tooltip

--- contents/course_content.php
+++ contents/course_content.php
@@ -89,8 +89,3 @@
         
     </section>
 </main>    
-
-<script>
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
-</script>
--- contents/professor_content.php
+++ contents/professor_content.php
@@ -144,9 +144,4 @@
             </div>
         <?php endif; ?>
     </section>
-</main>    
-
-<script>
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-const tooltipList = [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
-</script>
+</main>
