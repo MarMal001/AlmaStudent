f906352e97401ee7d3662542ab8659f887188643
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-06-01 22:10:13 +0200

    Added checks to see if the user can have access to the rating page

--- rating.php
+++ rating.php
@@ -4,16 +4,30 @@ require_once "init.php";
 $templateParams["title"] = "Rating";
 $templateParams["content"] = "rating_content.php";
 $templateParams["style"] = ["style.css"];
-$templateParams["type"] = $_GET["type"];
 
-//TODO --> add checks to see if it's a student tahat can rate (innest in an if else)
-
-if (isset($_GET["professor"])) {
-    $templateParams["professor"] = $_GET["professor"] . "@unibo.it";
-} else if (isset($_GET["course"])) {
-    $templateParams["course"] = $_GET["course"];
+if(!isStudent() || !isset($_GET["type"])) {
+    header("location: index.php");
 } else {
-    //TODO --> handle redirectioning
+        $dbh = $GLOBALS["dbh"];
+        $templateParams["type"] = $_GET["type"];
+        if (isset($_GET["professor"]) && $dbh->professorExists($_GET["professor"] . "@unibo.it")) {
+            $professor = $_GET["professor"] . "@unibo.it";
+            if ($dbh->canRateProfessor($user, $professor)[0]["existence"]) {
+                $templateParams["professor"] = $professor;
+            } else {
+                header("location: professor.php?professor=" . $_GET["professor"]);
+            } 
+        } elseif (isset($_GET["course"]) && $dbh->courseExists($_GET["course"])) {
+            if ($dbh->canRateCourse($user, $_GET["course"])[0]["existence"]) {
+                $templateParams["course"] = $_GET["course"];
+            } elseif ($dbh->courseExists($_GET["course"])) {
+                header("location: course.php?course=" . $_GET["course"]);
+            } else {
+                header("location: index.php");
+            }
+        } else {
+            header("location: index.php");
+        }
 }
 
 require "template/base.php";
