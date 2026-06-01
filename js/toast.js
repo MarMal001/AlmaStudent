4e956b3b752bb44426a25d336eaff5ab3878a1c7
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-06-01 22:31:45 +0200

    Added file to create a toast element

---
+++ js/toast.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toastEl = document.querySelector('.toast');
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+});
