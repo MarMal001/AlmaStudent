5c49e510551f4f801eea601e86e5c70893e4590c
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-06-01 21:08:25 +0200

    Added todoes

--- rating.php
+++ rating.php
@@ -6,11 +6,14 @@ $templateParams["content"] = "rating_content.php";
 $templateParams["style"] = ["style.css"];
 $templateParams["type"] = $_GET["type"];
 
+//TODO --> add checks to see if it's a student tahat can rate (innest in an if else)
+
 if (isset($_GET["professor"])) {
     $templateParams["professor"] = $_GET["professor"] . "@unibo.it";
-} 
-if (isset($_GET["course"])) {
+} else if (isset($_GET["course"])) {
     $templateParams["course"] = $_GET["course"];
+} else {
+    //TODO --> handle redirectioning
 }
 
 require "template/base.php";
