eef8998fa884f17e4251b13f76dca0b75d8bea5d
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-06-01 23:07:04 +0200

    Added a new check to see if the student can rate a course

--- contents/course_content.php
+++ contents/course_content.php
@@ -80,7 +80,7 @@
         <?php if (isStudent()): ?>
             <div class="d-flex justify-content-end mb-5 me-2">
                 <a class="btn btn-primary 
-                <?php if (!$dbh->canRateCourse($user, $courseId)[0]["existence"]) {
+                <?php if (!$dbh->canRateCourse($user, $courseId)[0]["existence"] || $dbh->courseIsAlreadyRated($user, $courseId)) {
                     echo "disabled";
                 } ; ?>" 
                 href="rating.php?type=course&course=<?php echo $courseId; ?>">Recensisci</a>
