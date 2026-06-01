3e9400de10aa1cbbb0348a9d6e6247df63b3d234
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-06-01 20:51:02 +0200

    feat: fixed return parameters in dbh method checkIfSubscribetToACourse

--- db/database.php
+++ db/database.php
@@ -330,7 +330,7 @@ class DatabaseHelper{
         $stmt->execute();
         $result = $stmt->get_result();
 
-        return $result->fetch_all(MYSQLI_ASSOC);
+        return $result->fetch_all(MYSQLI_ASSOC)[0]["subscribed"];
     }
 
     public function getProfessorRatings($professor) {
