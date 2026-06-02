a90e517f46368bb41711e029450209dd2f42dc08
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-06-02 17:50:18 +0200

    Created pages to permit the creation of a course's professors

---
+++ rating_professor.php
@@ -0,0 +1,25 @@
+<?php
+require_once "init.php";
+
+$templateParams["title"] = "Rating";
+$templateParams["content"] = "rating_professor_content.php";
+$templateParams["style"] = ["style.css"];
+
+if(!isStudent()) {
+    header("location: index.php");
+} else {
+        if (isset($_GET["course"]) && $dbh->courseExists($_GET["course"])) {
+            if ($dbh->canRateCourse($user, $_GET["course"])[0]["existence"] && !$dbh->professorsOfCourseAlreadyRated($user, $_GET["course"])) {
+                $templateParams["course"] = $_GET["course"];
+            } elseif ($dbh->courseExists($_GET["course"])) {
+                header("location: course.php?course=" . $_GET["course"]);
+            } else {
+                header("location: index.php");
+            }
+        } else {
+            header("location: index.php");
+        }
+}
+
+require "template/base.php";
+?>
