512c6ce0be8458a00c72a5cdee0153096108551c
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-06-04 18:50:23 +0200

    feat: added tooltip for showing when a student cannot reserve slot in reception page

--- reception_editable.php
+++ reception_editable.php
@@ -9,7 +9,7 @@ if (!isProfessor()) {
 $templateParams["title"] = "Reception";
 $templateParams["content"] = "reception_editable_content.php";
 $templateParams["style"] = ["style.css"];
-$templateParams["js"] = array("js/reception.js");
+$templateParams["js"] = array("js/reception.js", "js/tooltip.js");
 
 if (isset($_GET["message"])) {
     $templateParams["message"] = $_GET["message"];
