201934d26481079064c482dfb9c378614ae9addb
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-06-07 18:48:21 +0200

    feat: improved style

--- contents/update_profile_professor_content.php
+++ contents/update_profile_professor_content.php
@@ -1,52 +1,57 @@
 <?php $professorInfo = $dbh->getProfessorInfo($user)[0]; ?>
 <main>
-    <section>
-        <form action="handle_update_profile_professor.php" method="POST" enctype="multipart/form-data" class="w-75 mx-auto">
-            <ul>
-                <li>
-                    <label for="department" class="text-left form-label">
-                        Dipartimento
-                    </label>
-                </li>
-                <li>
-                    <input type="text" id="department" name="department" class="form-control rounded-pill" value="<?php echo $professorInfo["department"]; ?>" />
-                </li>
-                <li>
-                    <label for="seat" class="text-left form-label">
-                        Sede
-                    </label>
-                </li>
-                <li>
-                    <input type="text" id="seat" name="seat" class="form-control rounded-pill" value="<?php echo $professorInfo["campus"]; ?>" />
-                </li>
-                <li>
-                    <label for="infoReception" class="text-left">
-                        Info ricevimento
-                    </label>
-                </li>
-                <li>
-                    <textarea type="text" id="infoReception" name="infoReception" class="form-control" maxlength="500"><?php echo $professorInfo["infoReception"]; ?></textarea>
-                </li>
-                <li>
-                    <label for="profilePicture" class="text-left form-label">
-                        Immagine profilo
-                    </label>
-                </li>
-                <li>
-                    <input type="file" id="profilePicture" name="profilePicture" class="form-control rounded-pill" />
-                </li>
-                <li>
-                    <label for="currentProfilePicture" class="form-label">
-                        Immagine profilo corrente
-                    </label>
-                </li>
-                <li>
-                    <img src="<?php echo UPLOAD_DIR . "/professor/" . $dbh->getProfilePicture($_GET["professor"] . "@unibo.it"); ?>" alt=""></img>
-                </li>
-                <li>
-                    <input type="submit" class="btn btn-deepskyblue mt-3" value="Modifica" />
-                </li>
-            </ul>
-        </form>
+    <section class="container-fluid w-auto m-2 p-0 my-4">
+        <div class="bg-primary-subtle border border-secondary-subtle rounded text-black text-start w-75 mx-auto">
+            <div class="d-flex justify-content-between align-items-center fw-bold p-3">
+                <p class="m-0 p-2">Modifica account <?php echo strtolower($_GET["accountType"]); ?></p>
+            </div>
+            <form action="handle_update_profile_professor.php" method="POST" enctype="multipart/form-data" class="w-100">
+                <ul class="mb-0">
+                    <li>
+                        <label for="department" class="text-left form-label">
+                            Dipartimento
+                        </label>
+                    </li>
+                    <li>
+                        <input type="text" id="department" name="department" class="form-control rounded-pill" value="<?php echo $professorInfo["department"]; ?>" />
+                    </li>
+                    <li>
+                        <label for="seat" class="text-left form-label">
+                            Sede
+                        </label>
+                    </li>
+                    <li>
+                        <input type="text" id="seat" name="seat" class="form-control rounded-pill" value="<?php echo $professorInfo["campus"]; ?>" />
+                    </li>
+                    <li>
+                        <label for="infoReception" class="text-left">
+                            Info ricevimento
+                        </label>
+                    </li>
+                    <li>
+                        <textarea type="text" id="infoReception" name="infoReception" class="form-control" maxlength="500"><?php echo $professorInfo["infoReception"]; ?></textarea>
+                    </li>
+                    <li>
+                        <label for="profilePicture" class="text-left form-label">
+                            Immagine profilo
+                        </label>
+                    </li>
+                    <li>
+                        <input type="file" id="profilePicture" name="profilePicture" class="form-control rounded-pill" />
+                    </li>
+                    <li>
+                        <label for="currentProfilePicture" class="form-label">
+                            Immagine profilo corrente
+                        </label>
+                    </li>
+                    <li>
+                        <img src="<?php echo UPLOAD_DIR . "/professor/" . $dbh->getProfilePicture($_GET["professor"] . "@unibo.it"); ?>" alt=""></img>
+                    </li>
+                    <li class="d-flex justify-content-end">
+                        <input type="submit" class="btn btn-deepskyblue mt-3" value="Modifica" />
+                    </li>
+                </ul>
+            </form>
+        </div>
     </section>
 </main>
